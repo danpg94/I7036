68663b23eb18dbbcce98eccc602c91b4f79d5a72
Daniel Palma Garcia <danpalma94@gmail.com>
2023-09-11 22:21:07 -0600

    Threads

---
+++ threads/alexa_callback.py
@@ -0,0 +1,22 @@
+import csv, pdb, os
+from zipfile import ZipFile
+from io import TextIOWrapper, BytesIO
+import requests
+
+
+class AlexaCallback:
+    def __init__(self, max_urls=500):
+        self.max_urls = max_urls
+        self.seed_url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+        self.urls = []
+
+    def __call__(self):
+        resp = requests.get(self.seed_url, stream=True)
+        #pdb.set_trace()
+        with ZipFile(os.getcwd() + "\\threads\\chp4\\archive.zip", "r") as zf:
+            csv_filename = zf.namelist()[0]
+            with zf.open(csv_filename) as csv_file:
+                for _, website in csv.reader(TextIOWrapper(csv_file)):
+                    self.urls.append('http://' + website)
+                    if len(self.urls) == self.max_urls:
+                        break
